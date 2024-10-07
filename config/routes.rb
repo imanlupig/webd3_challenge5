2619f83a388cf6718af7e128d683164f353ebe17
Ian Manlupig <imanlupig@academic.rrc.ca>
2024-10-06 19:58:49 -0500

    added index view for Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "products/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :products, only: [ :index, :show ]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "products#index"
 end
